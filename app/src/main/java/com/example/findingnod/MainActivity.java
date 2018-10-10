02cdd706b508c5fa2e85341467ea0265800ed335
Alekc233 <43700907+Alekc233@users.noreply.github.com>
2018-10-10 23:11:24 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/findingnod/MainActivity.java
+++ app/src/main/java/com/example/findingnod/MainActivity.java
@@ -5,37 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "logs";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private static final String TAG = "logs";
+  public static final String EXTRA_MESSAGE = "com.example.findingnod.MESSAGE";
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void onButtonClick(View view) {
+
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText num1 = (EditText)findViewById(R.id.editText);
+    EditText num2 = (EditText)findViewById(R.id.editText2);
+
+    int n1 = Integer.parseInt(num1.getText().toString());
+    int n2 = Integer.parseInt(num2.getText().toString());
+    int res;
+
+    Log.d(TAG, "NOD OF NUM " + n1 + " " + n2);
+    int a = n1;
+    int b = n2;
+
+    while (b != 0){
+      int c = a % b;
+      a = b;
+      b = c;
     }
+    res = a;
+    Log.d(TAG, "NOD = " + res);
 
-    public void onButtonClick(View view) {
+    String message = Integer.toString(res);
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
 
-        EditText num1 = (EditText)findViewById(R.id.editText);
-        EditText num2 = (EditText)findViewById(R.id.editText2);
-        TextView result = (TextView)findViewById(R.id.result);
-
-        int n1 = Integer.parseInt(num1.getText().toString());
-        int n2 = Integer.parseInt(num2.getText().toString());
-        int res;
-
-        Log.d(TAG, "NOD OF NUM " + n1 + " " + n2);
-        int a = n1;
-        int b = n2;
-
-        while (b != 0){
-           int c = a % b;
-           a = b;
-           b = c;
-        }
-        res = a;
-        Log.d(TAG, "NOD = " + res);
-        result.setText(Integer.toString(res));
-    }
+  }
 }
