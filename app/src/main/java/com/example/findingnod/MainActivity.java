8028d7aec076ce07f94de29b9d368d39d9dfd1ac
Alekc233 <43700907+Alekc233@users.noreply.github.com>
2018-10-12 22:40:07 +1000

    save value

--- app/src/main/java/com/example/findingnod/MainActivity.java
+++ app/src/main/java/com/example/findingnod/MainActivity.java
@@ -10,10 +10,24 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "logs";
   public static final String EXTRA_MESSAGE = "com.example.findingnod.MESSAGE";
+  private static final String KEY_NUM1 = "N1";
+  private static final String KEY_NUM2 = "N2";
+  int n1,n2;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    Log.d(TAG, "Screen has been rotated");
+    Log.d(TAG, "a = "+ n1);
+    Log.d(TAG, "b = " + n2);
+    outState.putInt(KEY_NUM1, n1);
+    outState.putInt(KEY_NUM2, n2);
   }
 
   public void onButtonClick(View view) {
@@ -22,21 +36,28 @@ public class MainActivity extends AppCompatActivity {
     EditText num1 = (EditText)findViewById(R.id.editText);
     EditText num2 = (EditText)findViewById(R.id.editText2);
 
-    int n1 = Integer.parseInt(num1.getText().toString());
-    int n2 = Integer.parseInt(num2.getText().toString());
     int res;
 
-    Log.d(TAG, "NOD OF NUM " + n1 + " " + n2);
-    int a = n1;
-    int b = n2;
-
-    while (b != 0){
-      int c = a % b;
-      a = b;
-      b = c;
+    if( num1.getText().toString().equals("") || num2.getText().toString().equals("") ) {
+      Log.d(TAG, "there is no data entered");
+      res = 0;
     }
-    res = a;
-    Log.d(TAG, "NOD = " + res);
+      else {
+      n1 = Integer.parseInt(num1.getText().toString());
+      n2 = Integer.parseInt(num2.getText().toString());
+      Log.d(TAG, "NOD of " + n1 + " " + n2);
+      int a = n1, b = n2;
+      while (b != 0) {
+        int c = a % b;
+        a = b;
+        b = c;
+      }
+      res = a;
+      Log.d(TAG, "NOD is "+ res);
+    }
+
+
+
 
     String message = Integer.toString(res);
     intent.putExtra(EXTRA_MESSAGE, message);
